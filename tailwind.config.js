deca891678330492aaa8f91d1452c673ca2c7e31
vedant joshi <91942752+Vedantjoshi123@users.noreply.github.com>
2024-07-07 20:56:08 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust the paths as necessary
+    './public/index.html', 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
